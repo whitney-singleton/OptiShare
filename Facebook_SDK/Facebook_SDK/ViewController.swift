09c491cfc9141a7b52439254ae85e18a0953601d
jackson ohanian <ohanian3@illinois.edu>
2019-06-08 01:21:42 -0500

    Removed redundant submodules.

---
+++ Facebook_SDK/Facebook_SDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facebook_SDK
+//
+//  Created by Jackson on 5/29/19.
+//  Copyright © 2019 Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
